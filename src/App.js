7cf839f1e50303b9a263170d71a0291028c6db92
Kimrexhappy <115502393+Kimrexhappy@users.noreply.github.com>
2022-12-20 22:36:10 +0400

    Commit-1
    About and contact page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Hero from "./Hero";
-import { Grid } from "@mui/material";
-import PropertyCard from "./PropertyCard";
-import propertyData from "./propertyData";
-import Footer from "./Footer";
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-const theme = createTheme(
-  {
-    palette: {
-      primary: {
-        main: '#9E7676'
-      }
-    }
-  }
-)
-
+import About from "./About";
+import Contact from "./Contact";
 function App() {
-
   return (
-    <ThemeProvider theme={theme}>
-      <NavBar/>
-      <Hero/>
-      <Grid container justifyContent="center">
-        {propertyData.map(e => {
-          return <Grid item md={3}><PropertyCard title={e.title} price={e.price} description={e.description} img={e.img}/></Grid>
-        })}
-      </Grid>
-      <Footer/>
-    </ThemeProvider>
+    <>
+      <About />
+      <Contact />
+    </>
   );
 }
-
 export default App;
